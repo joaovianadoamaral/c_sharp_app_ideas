ebd55ac9335b08bdb1842c7de76868c55e082c17
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2024-07-09 20:09:55 -0300

    Primeiro programa para o respositorio AppIdeas. Implementacao de um conversor binario -> decimal com base em user storys. Implementacao bonus de mais de 8 caracteres.

---
+++ _1_begginer/bin2decimal/Bin2Decimal/CalculatorDecimalBinary.cs
@@ -0,0 +1,52 @@
+﻿namespace Bin2Decimal
+{
+    internal class CalculatorDecimalBinary
+    {
+
+        public static bool JustZeroOne(string str)
+        {
+            char[] Caracteres = str.ToCharArray();
+            bool DiferentZero, DiferentOne;
+
+            if (str == null)
+            {
+                Console.WriteLine("String Vazia");
+                return false;
+            }
+
+
+            for (int contador = 0; contador < Caracteres.Length; contador++)
+            {
+                DiferentZero = Caracteres[contador] != '0';
+                DiferentOne = Caracteres[contador] != '1';
+                if ( DiferentZero && DiferentOne)
+                {
+                    Console.WriteLine("Algo diferente de '0' ou '1' foi digitado. ");
+                    return false;
+                }
+            }
+
+                return true;
+        }
+
+        public static int Bin2Dec(string str)
+        {
+            char[] Caracteres = str.ToCharArray(); 
+            double NumDecimal = 0;
+            sbyte count = 0;
+
+            if (! JustZeroOne(str) ) { return (int) -1 ; }
+
+            for (int index = Caracteres.Length - 1; index >= 0; index--)
+            {
+                if (Caracteres[index] == '1')
+                {
+                    NumDecimal += Math.Pow(2, count);
+                }
+                count++;
+            }
+
+            return (int)NumDecimal;
+        }
+    }
+}
---
+++ _1_begginer/bin2decimal/Bin2Decimal/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Bin2Decimal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string StringBinary;
+            int IntDecimal;
+            const int InputError = -1;
+
+            Console.Write("Enter up to 8 binary digits: ");
+            StringBinary = Console.ReadLine();
+
+            IntDecimal = CalculatorDecimalBinary.Bin2Dec(StringBinary);
+
+            if (IntDecimal != InputError)
+            {
+                Console.WriteLine($"{IntDecimal} (b10)");
+            }
+
+        }
+    }
+}
+
+/*
+story user:
+    -  User can enter up to 8 binary digits in one input field
+    - User must be notified if anything other than a 0 or 1 was entered
+    - User views the results in a single output field containing the decimal (base 10) equivalent of the binary number that was entered             
+*/
+
+// bonus features:  User can enter a variable number of binary digits
